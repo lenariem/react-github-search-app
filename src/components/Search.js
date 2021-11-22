d43245bde35782c2d2caff0418fc2433db1e21ad
unknown <olena.denysova88@gmail.com>
2021-11-22 16:27:29 +0100

    home page mockup

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Search() {
+    return (
+        <div className="form-group mb-3">
+            <input 
+                type="text"
+                className="form-control"
+                placeholder="Enter a github name..."
+            />
+            
+        </div>
+    )
+}
--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import Card from "../components/Card";
+import Search from "../components/Search";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const cards = new Array(15).fill("").map((_, i) => i);
+  console.log(cards);
+  return (
+    <>
+      <Search />
+      <div className="row">
+        {cards.map((card) => {
+          return (
+            <div className="col-sm-4 mb-4" key={card}>
+              <Card />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
