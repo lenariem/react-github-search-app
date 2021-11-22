dafc4befcf2238b03f124a67ed4113de83b0161f
unknown <olena.denysova88@gmail.com>
2021-11-22 15:53:59 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="container pt-4">
-     <h1>Hello</h1>
+      <h1>Hello</h1>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+         <div className="navbar-brand">
+           Github Search 
+         </div> 
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a href="!#" className="nav-link">Main</a>
+                </li>
+                <li className="nav-item">
+                    <a href="!#" className="nav-link">Info</a>
+                </li>
+            </ul>  
+
+        </nav>
+    )
+}
